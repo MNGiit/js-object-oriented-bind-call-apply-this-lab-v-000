23f331e5bdea248b32fc07c73b2aa6dee87d165d
Mohammed Noori <kmanoorib@gmail.com>
2019-03-02 03:10:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function justInvoke(fn, args){
 function setThisWithCall(fn, thisValue, arg){
   return fn.call(thisValue, arg);
 }
+
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue, args);
+}
